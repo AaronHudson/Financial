dfea45bce37a49abbedf0e74b0ab98f1261fb251
AaronHudson <aaron.hudson18@gmail.com>
2015-09-17 10:50:48 -0500

    Changed flag on category.Limit

--- Financial.Web/Models/Category.cs
+++ Financial.Web/Models/Category.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -17,7 +18,7 @@ namespace Financial.Web.Models
         public virtual Budget Budget { get; set; }
         [Required]
         [RegularExpression(@"^(\$?\d{1,3}(,?\d{3})?(\.\d\d?)?|\(\$?\d{1,3}(,?\d{3})?(\.\d\d?)?\))$", ErrorMessage = "I'm sorry, this doesn't seem to match a US currency.")]
-        [Display(Name = "Goal")]
+        [DisplayName("Goal")]
         public decimal Limit { get; set; }
         [Required]
         public string Title { get; set; }
