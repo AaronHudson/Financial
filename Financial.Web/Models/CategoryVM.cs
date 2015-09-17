7aae04da810ede4f3c5c5a988738ff0933c9f1d4
AaronHudson <aaron.hudson18@gmail.com>
2015-09-17 10:57:33 -0500

    Added displayname flag to categoryVM.limit

--- Financial.Web/Models/CategoryVM.cs
+++ Financial.Web/Models/CategoryVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace Financial.Web.Models
     {
         [Required]
         [RegularExpression(@"^(\$?\d{1,3}(,?\d{3})?(\.\d\d?)?|\(\$?\d{1,3}(,?\d{3})?(\.\d\d?)?\))$", ErrorMessage ="I'm sorry, this doesn't appear to match a US currency.")]
+        [DisplayName("Goal")]
         public string Limit { get; set; }
         [Required]
         public string Title { get; set; }
